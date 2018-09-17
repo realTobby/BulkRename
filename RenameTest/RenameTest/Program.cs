cc1793f20ca9c15b3beb1a32897145451220df5f
xSillusx <tkattanek00@gmail.com>
2018-09-17 14:37:35 +0200

    init, currently for renaming bulks of images

---
+++ RenameTest/RenameTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Path to the folder:");
+            var folderPath = Console.ReadLine();
+
+            Console.WriteLine("Starting count:");
+            var count = Convert.ToInt32(Console.ReadLine());
+
+            var files = System.IO.Directory.GetFiles(folderPath);
+
+            string destinationFolder = folderPath + "//renamedFiles";
+            System.IO.Directory.CreateDirectory(destinationFolder);
+
+            foreach(var item in files)
+            {
+                count++;
+                System.IO.File.Copy(item, destinationFolder + "//" + count + ".png");
+
+
+
+            }
+        }
+    }
+}
